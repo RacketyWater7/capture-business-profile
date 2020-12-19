84d13c8ecf89dfa960f022657d4101ddac796d2f
Haseeb Bajwa <jonfoo47@hotmail.com>
2020-12-20 00:43:34 +0500

    updated animation and OurWord JS's

--- src/animation.js
+++ src/animation.js
@@ -46,3 +46,19 @@ export const photoAnim = {
     transition: { ease: "easeOut", duration: 0.75 },
   },
 };
+export const lineAnim = {
+  hidden: { width: "0%" },
+  show: { width: "100%", transition: { duration: 1 } },
+};
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
+};
