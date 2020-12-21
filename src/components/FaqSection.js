07aa71aae93ef52268122421748482635257b112
Haseeb Bajwa <jonfoo47@hotmail.com>
2020-12-21 17:58:04 +0500

    added Toggle.js and animated FaqSection.js

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,60 +1,53 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
-  const [faqToggle, setFaqToggle] = useState(false);
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div onClick={() => setFaqToggle(!faqToggle)} className="question">
-        <h4>How Do I Start</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          {faqToggle && (
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing
               Necessitatibus, neque.
             </p>
-          )}
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing Necessitatibus,
-            neque.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing Necessitatibus,
-            neque.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What products do you offer.</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing Necessitatibus,
-            neque.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What products do you offer.">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
